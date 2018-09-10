adc354fe228880cff5065ad3cf20d72244236b4a
Finn Skiler <awesomefinnskiler@gmail.com>
2018-09-10 10:52:16 +0530

    ready chalk fix

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,5 @@
-const chalk = require('chalk');
 module.exports = client => {
-    console.log(chalk.bgGreen.black(`I\'m online!`));
+    console.log(`I\'m online!`);
 
     client.user.setActivity("The Whole Mortal Universe", {type: "WATCHING"});
 }
